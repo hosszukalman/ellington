ce08cf9955a624c3332908bba088586525ac4e7c
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-02-20 10:41:15 +0100

    Add search box

---
+++ template.php
@@ -0,0 +1,35 @@
+<?php
+// $Id$
+/**
+ * @file
+ *
+ *
+ *
+ * @author Kálmán Hosszu - hosszu.kalman@gmail.com - http://www.kalman-hosszu.com
+ */
+
+/**
+ * Implementation of hook_perprocess_page().
+ *
+ * Chcek search box, and a special class to body_classes.
+ *
+ * @param array $vars
+ */
+function phptemplate_preprocess_page(&$vars) {
+  if (!empty($vars['search_box'])) {
+    $vars['body_classes'] .= ' search-box';
+  }
+}
+
+/**
+ * Override search form theme.
+ *
+ * Disable title and modify submit button to image.
+ *
+ * @param array $form
+ * @return The rendered form
+ */
+function phptemplate_search_theme_form($form) {
+  unset($form['search_theme_form']['#title']);
+  return drupal_render($form);
+}
